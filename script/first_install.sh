0ac70d29095c048479b1e736c7b6728af66007be
pierrick Bournez <84439209+gardiens@users.noreply.github.com>
2024-11-10 13:15:25 +0100

    Change first installation

--- script/first_install.sh
+++ script/first_install.sh
@@ -5,3 +5,4 @@ source venv/bin/activate  # for linux
 venv\Scripts\activate.bat  # for windows
 pip install -r requirements.txt
 pre-commit install
+#pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116 #uncomment if you areon DGX
