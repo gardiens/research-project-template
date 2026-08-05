ae4f923b37841c9d2aba18ed342ae4648d7b8354
gardiens <pierrick.bournez@gmail.com>
2026-08-05 11:28:19 +0200

    :fire: updated the dev_requirements as well as pre-commit and agents .md

--- tests/test_import.py
+++ tests/test_import.py
@@ -16,17 +16,3 @@ def test_import():
 
 def test_model():
     pass 
-
-
-from omegaconf import DictConfig, open_dict
-
-
-def test_instantiate(cfg_train: DictConfig):
-    import hydra
-    from hydra import compose, initialize
-    from omegaconf import DictConfig, open_dict
-    instantiator = hydra.utils.instantiate
-    logger: "SummaryWriter" = instantiator(cfg_train.logger)
-    base_datamodule: "BaseDataModule" = hydra.utils.instantiate(cfg_train.dataset)
-    criterion = instantiator(cfg_train.loss,)
-    net = instantiator(cfg_train.model,)
